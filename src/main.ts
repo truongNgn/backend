7b0abeedcae60760767c86bac23331f5575af334
lethanhdat <lethanhdat1993@gmail.com>
2026-06-16 11:04:44 +0700

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'http://localhost:3000' });
   app.useGlobalPipes(new ValidationPipe());
+  // Swagger / OpenAPI setup - enabled in all environments
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3001);
 }
 bootstrap();
