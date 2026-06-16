74d10bb8dfe15fae9e6e2b16790952ad8989c828
minhnhat <minhnhatnhc@gmail.com>
2026-06-16 13:24:53 +0700

    add dto detail

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,26 @@
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Email address of the user, must be unique',
+    example: 'john.doe@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'First name of the user',
+    example: 'John',
+  })
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty({
+    description: 'Last name of the user',
+    example: 'Doe',
+  })
   @IsString()
   @IsNotEmpty()
   lastName: string;
