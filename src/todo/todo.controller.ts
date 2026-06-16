62959e316ada1bf2d540fbd818be132c10fe2631
truongNgn <truong.nhat3040@gmail.com>
2026-06-16 08:42:55 +0700

    CRUD function

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { Todo } from './todo.entity';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post() //C -> CREATE
+  create(@Body('title') title: string, @Body('completed') completed?: boolean): Promise<Todo> {
+    return this.todoService.create(title, completed);
+  }
+
+  @Get() //R -> READ ALL
+  findAll(): Promise<Todo[]> {
+    return this.todoService.findAll();
+  }
+
+  @Patch(':id') //U -> UPDATE by id
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() attrs: Partial<Todo>,
+  ): Promise<Todo> {
+    return this.todoService.update(id, attrs);
+  }
+
+  @Delete(':id') //D -> DELETE by id
+  remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.todoService.remove(id);
+  }
+}
+
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './todo.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo)
+    private readonly todoRepository: Repository<Todo>,
+  ) {}
+
+  async create(title: string, completed?: boolean): Promise<Todo> { //CREATE
+    const todo = this.todoRepository.create({ title, completed });
+    return this.todoRepository.save(todo);
+  }
+
+  async findAll(): Promise<Todo[]> { //READ ALL
+    return this.todoRepository.find({
+      order: {
+        createdAt: 'DESC',
+      },
+    });
+  }
+
+  async findOne(id: number): Promise<Todo> {  //READ SINGLE by id
+    const todo = await this.todoRepository.findOneBy({ id });
+    if (!todo) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    return todo;
+  }
+
+  async update(id: number, attrs: Partial<Todo>): Promise<Todo> { //UPDATE by id 
+    const todo = await this.findOne(id);
+    Object.assign(todo, attrs);
+    return this.todoRepository.save(todo);
+  }
+
+  async remove(id: number): Promise<void> { //DELETE by id
+    const todo = await this.findOne(id);
+    await this.todoRepository.remove(todo);
+  }
+}
+
